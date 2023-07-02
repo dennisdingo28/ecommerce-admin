984e7341ae28d53c397cd4d665631261ad70ad79
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-07-02 20:22:06 +0300

    prisma relationships

--- app/(dashboard)/[storeId]/(routes)/settings/components/Settings-Form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/Settings-Form.tsx
@@ -14,6 +14,9 @@ import { toast } from "react-hot-toast";
 import axios from "axios";
 import { useParams } from "next/navigation";
 import { useRouter } from "next/navigation";
+import { AlertModal } from "@/components/ui/modals/alert-modal";
+import { ApiAlert } from "@/components/ui/api-alert";
+import { useOrigin } from "@/hooks/useOrigin";
 
 interface SettingsFormProps {
     initialData: Store;
@@ -21,7 +24,6 @@ interface SettingsFormProps {
 
 const formSchema = z.object({
     name: z.string().min(1),
-    
 });
 
 type SettingsFormValues = z.infer<typeof formSchema>;
@@ -30,6 +32,7 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 export const SettingsForm: FC<SettingsFormProps> = ({initialData}) => {
     const params = useParams();
     const router = useRouter();
+    const origin = useOrigin();
 
     const [open,setOpen] = useState(false);
     const [loading,setLoading] = useState(false);   
@@ -51,9 +54,23 @@ export const SettingsForm: FC<SettingsFormProps> = ({initialData}) => {
             setLoading(false);
         }
     }
-
+    const onDelete = async()=>{
+        try {
+            setLoading(true);
+            await axios.delete(`/api/stores/${params.storeId}`);
+            router.refresh();
+            router.push("/")
+            toast.success('Store was successfully deleted.');
+        } catch (error) {
+            toast.error('Make sure you removed all products and categories first.');
+        }finally{
+            setLoading(false);
+            setOpen(false);
+        }
+    }
     return (
         <>
+            <AlertModal isOpen={open} onClose={()=>setOpen(false)} onConfirm={onDelete} loading={loading} />
             <div className="flex items-center justify-between">
                 <Heading title="Settings" description="Manage store preferences"/>
                 <Button variant="destructive" size="icon" onClick={()=>setOpen(true)} disabled={loading}>
@@ -77,6 +94,8 @@ export const SettingsForm: FC<SettingsFormProps> = ({initialData}) => {
                     <Button disabled={loading} className="ml-auto" type="submit">Save Changes</Button>
                 </form>
             </Form>
+            <Separator/>
+            <ApiAlert title="NEXT_PUBLIC_API_URL" description={`${origin}/api/${params.storeId}`} variant="public"/>
         </>
     )
 }
